56c27e6138d566532e55f79334e49a5b6daf8772
Angellomon <angelmtzdiaz@gmail.com>
2022-01-14 15:11:57 -0600

    feat(site): despliegue de imágenes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let images = [];
+
+	onMount(async () => {
+		const arr = [];
+		const res = await fetch('/assets');
+
+		const data = await res.json();
+
+		console.log(data);
+
+		for (let asset of data.assets) {
+			arr.push(asset.image_url);
+		}
+
+		images = arr;
+	});
+
+	console.log(images);
+
+	$: console.log(images);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each images as image}
+	<img src={image} alt="img" />
+{/each}
