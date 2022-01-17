cbe802987dcd18966e76fac9223eeaa13e89b0b9
Angellomon <angelmtzdiaz@gmail.com>
2022-01-17 17:52:10 -0600

    feat(lib): componentes para desplegar la coleccion de assets

---
+++ src/lib/components/AssetFig.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Asset } from '$lib/schemas';
+
+	export let asset: Asset;
+	let ellipsis = true;
+
+	const toggleParagraph = () => {
+		ellipsis = !ellipsis;
+	};
+	console.log(asset.external_link);
+</script>
+
+<figure>
+	<img src={asset.image_url} alt={asset.token_id} />
+	<figcaption>
+		<p class:ellipsis on:click={toggleParagraph}>{asset.description}</p>
+		<a href={asset.permalink}>OpenSea</a>
+	</figcaption>
+</figure>
+
+<style>
+	img {
+		max-width: 7em;
+		height: auto;
+	}
+	figure {
+		max-width: 7em;
+	}
+	figcaption p.ellipsis {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+</style>
---
+++ src/lib/components/Collection.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Asset } from '$lib/schemas';
+	import AssetFig from './AssetFig.svelte';
+	export let assets: Asset[] = [];
+</script>
+
+<section>
+	{#each assets as asset}
+		<AssetFig {asset} />
+	{/each}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
